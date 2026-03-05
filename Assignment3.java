470b9d5b3ddb92c63f4852449bd7fae4137fcf70
drakshakb <30766615+drakshakb@users.noreply.github.com>
2026-03-05 15:59:17 +0530

    Updated

---
+++ Assignment3.java
@@ -0,0 +1,15 @@
+class Assignment3 {
+public static int[] removeNegativeNumbers(int[] arr){
+// Implement logic here
+
+  return null;
+}
+
+public static void main(String[] args) {
+int[] arr = {-5,3,-2,7,8,-1,4};
+int[] result = removeNegativeNumbers(arr);
+for(int i=0; i<result.length; i++){
+System.out.println(result[i]);
+}
+}
+}
